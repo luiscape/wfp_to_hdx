c6550e5165a3f61ef60787a760e785c12ab9d4ab
Luis Capelo <luiscape@gmail.com>
2014-08-14 16:17:13 -0400

    adding scraper structure

--- code/scraper.R
+++ code/scraper.R
@@ -1,9 +1,9 @@
 ## Script to transform WFP data from the VAM unit into the HDX format.
-library(xlsx)
 library(reshape2)
+source('code/write_tables.R')
 
 # loading data from source
-source_data <- read.xlsx('data/source/fcsforhdx.xlsx', sheetIndex = 1)
+source_data <- read.csv('data/source/fcsforhdx.csv')
 
 # create indicators
 indicator <- data.frame(indID = c('CHD.B.FOS.04.T6', 'CHD.B.FOS.05.T6', 'CHD.B.FOS.06.T6'), name = c('Percentage of households with poor food consumption', 'Percentage of households with borderline food consumption','Percentage of households with acceptable food consumption'), units = c('Percent'))
@@ -19,7 +19,16 @@ value <- melt(value)
 value$Metadata <- NULL
 value$is_number <- 1
 names(value) <- c('region', 'admin1', 'admin2', 'period', 'source', 'indID', 'value', 'is_number')
+value$region <- ifelse(value$admin2 != "", as.character(value$admin2), as.character(value$region))
+value$region <- ifelse(value$admin1 != "" & value$admin2 == "", as.character(value$admin1), as.character(value$region))
 value$dsID <- 'wfp-vam'
+value$admin1 <- NULL
+value$admin2 <- NULL
+
+# writing tables
+writeTables(df = indicator, table_name = 'indicator', db = 'scraperwiki')
+writeTables(df = dataset, table_name = 'dataset', db = 'scraperwiki')
+writeTables(df = value, table_name = 'value', db = 'scraperwiki')
 
 # storing CSV
 write.csv(value, 'data/value.csv', row.names = F)
--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
 Rscript code/scraper.R
+mv scraperwiki.sqlite ../
+zip -r data/wfp-vam.zip data/indicator.csv data/value.csv data/dataset.csv
+mv data/wfp-vam.zip http/csv.zip
