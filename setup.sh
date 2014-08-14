6332b65f3ef3ee79c1b49bfa620b7626e4f4a808
Luis Capelo <luiscape@gmail.com>
2014-08-14 16:19:47 -0400

    fixing setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 cd ..
 rm -rf -- tool
-mv hdxscraper-ors tool
+mv wfp_to_hdx tool
 #crontab -l | { cat; echo "@daily bash tool/run.sh"; } | crontab -
 printf "Now install the packages reshape2 and sqldf in R.\n"
 R
